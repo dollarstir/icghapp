d16bc2295b9d8ab56f78b1c3fe6a65ca4e3f9bb9
dollarstir <kpin463@gmail.com>
2022-05-01 08:39:01 +0000

    sdfsdf

--- main/default/acceptgroup.php
+++ main/default/acceptgroup.php
@@ -6,11 +6,11 @@ if (isset($id)) {
     $d = mysqli_query($conn, "UPDATE gp SET status ='approved' WHERE id ='$id' ");
     if ($d) {
         echo '<script>alert("Request Approved");
-            window.location="gptest.php";
+            window.location="gtest.php";
         </script>';
     } else {
         echo '<script>alert("Something went wrong");
-        window.location="gptest.php";
+        window.location="gtest.php";
     </script>';
     }
 }
